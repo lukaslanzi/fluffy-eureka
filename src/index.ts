5c6cc257d4e16b028d92c4da0681c36aeb1463a1
lukaslanzi <lukaslanzi14@gmail.com>
2022-03-07 22:24:21 -0300

    pruebas con la foto ashei

--- src/index.ts
+++ src/index.ts
@@ -5,15 +5,15 @@ const app = new Application({
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 640,
-	height: 480
+	width: 1620,
+	height: 720
 });
 
 const clampy: Sprite = Sprite.from("clampy.png");
 
 clampy.anchor.set(0.5);
 
-clampy.x = app.screen.width / 2;
-clampy.y = app.screen.height / 2;
+clampy.x = 800;
+clampy.y = 300;
 
 app.stage.addChild(clampy);
