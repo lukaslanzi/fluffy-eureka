0dc58b7221215beb305495a73910f10a3a979713
lukaslanzi <lukaslanzi14@gmail.com>
2022-03-22 21:40:43 -0300

    tarea2

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,53 @@
-import { Application, Sprite } from 'pixi.js'
+import { Application, Container, Sprite } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
 	resolution: window.devicePixelRatio || 1,
 	autoDensity: true,
 	backgroundColor: 0x6495ed,
-	width: 1820,
-	height: 780
+	width: 1280,
+	height: 720
 });
 
-const clampy: Sprite = Sprite.from("./perro pug.jpg");
+window.addEventListener("resize" , ()=>{
+	console.log("resized!")
+	const scaleX = window.innerWidth / app.screen.width;
+	const scaleY = window.innerHeight / app.screen.height;
+	const scale = Math.min (scaleX , scaleY);
 
-console.log("Hola profe"+ "arreglé la tarea")
+	const gamewidht = Math.round(app.screen.width * scale);
+	const gameheight = Math.round(app.screen.height * scale);
 
-clampy.anchor.set(0.5);
+	const marginHorizontal = Math.floor((window.innerWidth - gamewidht) /2);
+	const marginVertical = Math.floor((window.innerHeight - gameheight) /2);
 
-clampy.x = 700;
-clampy.y = 300;
+	app.view.style.marginLeft = marginHorizontal + "px";
+	app.view.style.marginRight = marginHorizontal + "px";
+	
+	app.view.style.marginTop = marginVertical + "px";
+	app.view.style.marginBottom = marginVertical + "px";
 
-app.stage.addChild(clampy);
+
+	app.view.style.width = gamewidht + "px";
+	app.view.style.height = gameheight + "px";
+});
+window.dispatchEvent (new Event ("resize"));
+
+const clampy: Sprite = Sprite.from("perro pug.jpg");
+
+const anteojos: Sprite = Sprite.from ("./anteojos facheros png.png");
+
+anteojos.position.set (70,10);
+
+anteojos.scale.set (0.2);
+
+const PugConAnteojos: Container = new Container ();
+
+PugConAnteojos.addChild (clampy);
+PugConAnteojos.addChild (anteojos);
+
+PugConAnteojos.scale.set (2);
+PugConAnteojos.x = (200);
+PugConAnteojos.y = (200);
+
+app.stage.addChild(PugConAnteojos);
